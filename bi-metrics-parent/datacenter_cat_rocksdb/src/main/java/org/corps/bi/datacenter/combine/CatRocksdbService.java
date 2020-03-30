1ee04b28fbef2a32703c4a15fe157a9a11a8daa0
郭建炯 <guojianjiong@hoolai.com>
2020-03-30 17:17:30 +0800

    升级版本号

--- bi-metrics-parent/datacenter_cat_rocksdb/src/main/java/org/corps/bi/datacenter/combine/CatRocksdbService.java
+++ bi-metrics-parent/datacenter_cat_rocksdb/src/main/java/org/corps/bi/datacenter/combine/CatRocksdbService.java
@@ -3,7 +3,7 @@ package org.corps.bi.datacenter.combine;
 import java.util.List;
 import java.util.Map;
 
-import org.corps.bi.dao.rocksdb.CatMetricsInRocksdb;
+import org.corps.bi.dao.rocksdb.CatMetricsInRocksdbV2;
 import org.corps.bi.tools.util.JSONUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,12 +14,12 @@ public class CatRocksdbService {
 	
 	public void cat(String metric,String day) {
 		
-		Map<String,Object> messAll=CatMetricsInRocksdb.getInstance().cat(metric, day);
+		Map<String,Object> messAll=CatMetricsInRocksdbV2.getInstance().cat(metric, day);
 		LOGGER.info("cat metric:{} mess:{}",metric,JSONUtils.toJSON(messAll));
 	}
 	
 	public void cat(String day) {
-		List<Map<String,Object>> messAll=CatMetricsInRocksdb.getInstance().catAll(day);
+		List<Map<String,Object>> messAll=CatMetricsInRocksdbV2.getInstance().catAll(day);
 		LOGGER.info("cat metric:{} mess:{}","all",JSONUtils.toJSON(messAll));
 	}
 	
